705521279c1d5ecdf81c1ae7c5565b974db6a664
amit kundoo <amitkundoo90@gmail.com>
2024-04-30 16:54:35 +0530

    Rubocop fix

--- spec/factories/person.rb
+++ spec/factories/person.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory(:person) do
     name { Faker::Internet.name }
-    title { Faker::Name.last_name()}
+    title { Faker::Name.last_name }
   end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,10 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-
-
-
 end
